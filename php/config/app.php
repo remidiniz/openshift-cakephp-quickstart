e5ebf0e0ccdbf05670439b23fa4c51cf2c62fdbb
Remi Diniz <remidiniz@gmail.com>
2015-09-18 16:30:57 +0100

    Initial

--- php/config/app.php
+++ php/config/app.php
@@ -207,42 +207,20 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
-            'persistent' => false,
-            'host' => 'localhost',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'nonstandard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'driver' => 'Cake\Database\Driver\Postgres',
+            'persistent' => true,
+            'host' => getenv('OPENSHIFT_POSTGRESQL_DB_HOST'),
+            'port' => getenv('OPENSHIFT_POSTGRESQL_DB_PORT'),
+            'username' => getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+            'password' => getenv('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+            'database' => 'starad24_core',
+            'schema' => 'public',
             'encoding' => 'utf8',
-            'timezone' => 'UTC',
+            'timezone' => 'Europe/Berlin',
             'cacheMetadata' => true,
-            'log' => false,
-
-            /**
-             * Set identifier quoting to true if you are using reserved words or
-             * special characters in your table or column names. Enabling this
-             * setting will result in queries built using the Query Builder having
-             * identifiers quoted when creating SQL. It should be noted that this
-             * decreases performance because each query needs to be traversed and
-             * manipulated before being executed.
-             */
-            'quoteIdentifiers' => false,
-
-
-            /**
-             * During development, if using MySQL < 5.6, uncommenting the
-             * following line could boost the speed at which schema metadata is
-             * fetched from the database. It can also be set directly with the
-             * mysql configuration directive 'innodb_stats_on_metadata = 0'
-             * which is the recommended value in production environments
-             */
-            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+            'log' => true,
+            'flags' => [],
+            'init' => []
         ],
 
         /**
